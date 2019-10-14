a74f04452a53b551f0aebd85603e765643e159ef
solatale <solatale@gmail.com>
2019-10-14 11:14:12 +0800

    20191014T1114

--- batchrun.sh
+++ batchrun.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
-# example: bash batchrun.sh 065 424
-simcatname="Cssos_magsim_SNR_tile_"$1"_"$2"_simuaper.txt"
-cat2lephare=${simcatname%.*}"_toLephare.txt"
-lephareout="OutFitCssos_tile_"$1"_"$2".out"
-#python3 hst814simsed_phutil_mp.py $1
-python3 magsim2lephare.py $simcatname $2
-export LEPHAREWORK='/mnt/work/CSSOS/lephare_dev/sim2pht-Z_'$2
+# example: bash batchrun.sh 424
+schemecode=$1
+#echo $schemecode
+mergelist="Cssos_magsim_toMerge.lst"
+rm $mergelist
+tiles=''
+for tile in 053 064 065 066 077
+    do
+        simcatname="Cssos_magsim_SNR_tile_"$tile"_"$schemecode".txt"
+        python3 hst814simsed_phutil_mp_debug.py $tile
+        ls $simcatname >> $mergelist
+        tmp=$tiles
+        tiles=$tmp$tile
+        sleep 3
+    done
+mergedfile="Cssos_magsim_SNR_tile"$tiles"_"$schemecode".txt"
+echo "Files to be merged:"
+cat $mergelist ; echo ''
+python3 SimuCataMerge.py $mergelist $mergedfile
+sleep 3
+python3 magsim2lephare.py $mergedfile $schemecode
+sleep 3
+tolephare_merged=${mergedfile%.*}"_toLephare.txt"
+lephareout="OutFitCssos_tile"$tiles"_"$schemecode".out"
+export LEPHAREWORK='/mnt/work/CSSOS/lephare_dev/sim2pht-Z_'$schemecode
 echo $LEPHAREWORK
-cp $cat2lephare $LEPHAREWORK
+cp $tolephare_merged $LEPHAREWORK
 cd $LEPHAREWORK
-$LEPHAREDIR/source/zphota -c $LEPHAREWORK/config/cssos_zphot_cssbands.para -CAT_IN $LEPHAREWORK/$cat2lephare -CAT_OUT $LEPHAREWORK/$lephareout -INP_TYPE M -CAT_TYPE LONG -TRANS_TYPE 1 -CAT_FMT MEME -SPEC_OUT NO -ZFIX NO
+$LEPHAREDIR/source/zphota -c $LEPHAREWORK/config/cssos_zphot_cssbands.para -CAT_IN $LEPHAREWORK/$tolephare_merged -CAT_OUT $LEPHAREWORK/$lephareout -INP_TYPE M -CAT_TYPE LONG -TRANS_TYPE 1 -CAT_FMT MEME -SPEC_OUT YES -ZFIX NO
+sleep 3
 python3 /work/CSSOS/filter_improve/fromimg/windextract/results/zstat.py $lephareout
-cd -
-cp $LEPHAREWORK/$lephareout ./results/
-cp $LEPHAREWORK/${lephareout%.*}".png" ./results/
+sleep 3
+cd /work/CSSOS/filter_improve/fromimg/windextract/
+mv $mergelist ./results/
+mv $mergedfile ./results/
+mv $tolephare_merged ./results/
+mv $LEPHAREWORK/$lephareout ./results/
+mv $LEPHAREWORK/${lephareout%.*}".png" ./results/
+
+# python3 snr_check.py $mergedfile
